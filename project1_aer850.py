2628192ab8dbfe1645b418271947e8874ddecd79
aerospace-curling <emily.peelar@torontomu.ca>
2025-09-29 22:23:57 -0400

    Step 1 Update September 29th
    Reading the data and imported pandas library

---
+++ project1_aer850.py
@@ -0,0 +1,10 @@
+#Project 1 AER 850
+
+#Part 1: Data Processing
+
+#reading the data and importing the pandas library
+import pandas as pd
+data=pd.read_csv("data/Project 1 Data.csv")
+print(data.head())
+
+
