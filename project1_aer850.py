c06e436ab7d8096a4c63f67ead1094ec962db69b
aerospace-curling <emily.peelar@torontomu.ca>
2025-09-29 23:18:02 -0400

    Progress with Data Visualization and Plots
    Worked on experimenting with various types of plots for the dataset including scatter and lune plots

--- project1_aer850.py
+++ project1_aer850.py
@@ -2,9 +2,47 @@
 
 #Part 1: Data Processing
 
-#reading the data and importing the pandas library
+#reading the data and importing the pao"ndas library
 import pandas as pd
 data=pd.read_csv("data/Project 1 Data.csv")
-print(data.head())
+#To check that reading the data is working correctly:
+#print(data.head())
 
 
+#Part 2: Data Visualization
+
+#importing the modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Creating a line graph of the Step number vs. movement in the Z direction
+plt.figure()
+plt.plot(data["Step"], data["Z"], color="blue", marker="o")
+plt.title("Z-axis Progression Across Steps")
+plt.xlabel("Step Number")
+plt.ylabel("Z-axis Progression")
+plt.show
+
+#Creating a line graph of the Step number vs. movement in the Y direction
+plt.figure()
+plt.plot(data["Step"], data["Y"], color="black", marker="o")
+plt.title("Y-axis Progression Across Steps")
+plt.xlabel("Step Number")
+plt.ylabel("Y-axis Progression")
+plt.show
+
+#Creating a line graph of the Step number vs. movement in the X direction
+plt.figure()
+plt.plot(data["Step"], data["X"], color="green", marker="o")
+plt.title("X-axis Progression Across Steps")
+plt.xlabel("Step Number")
+plt.ylabel("X-axis Progression")
+plt.show
+
+#Creating a scatter plot
+plt.figure()
+plt.scatter(data["Step"], data["Z"], c=data["Step"], cmap="Blues")
+plt.title("Scatterplot of Z-movement across the Steps")
+plt.xlabel("Step Number")
+plt.ylabel("Z-axis movement")
+plt.show()
