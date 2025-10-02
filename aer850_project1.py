f4824104fa40ce87378211fd884ff6647e193e8a
aerospace-curling <emily.peelar@torontomu.ca>
2025-10-01 20:41:38 -0400

    Updated Part 2
    Made some modifications to the naming

--- aer850_project1.py
+++ aer850_project1.py
@@ -1,6 +1,6 @@
 #Project 1 AER 850
 
-#Part 1: Data Processing
+#Part 1: Data Processing and splitting the data into test and train data
 
 #reading the data and importing the pandas library
 import pandas as pd
@@ -33,4 +33,46 @@ print(data["Step"].value_counts() / len(data))
 y_train = strat_data_train['Step']
 x_train = strat_data_train.drop(columns=['Step'])
 y_test = strat_data_test['Step']
-x_test = strat_data_test.drop(columns=['Step'])            axs[i,j].hist(data[items[i]], bins=20)
+x_test = strat_data_test.drop(columns=['Step'])
+
+#Part 2: Data Visualization
+
+#importing the modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Obtaining statistical data about the dataset and printing it
+print("This is statistical data about the dataset:\n")
+print(strat_data_train.describe())
+
+#Creating an array of the items to be compared with the subplots
+items = ['X','Y','Z','Step']
+
+#creating a plot with subplots to show the trends in data. The size of the plot is defined by the length of the matrix, Items
+fig, axs = plt.subplots(len(items),len(items))
+
+
+  
+#creating the subplots through the use of loops
+for i in range(len(items)):
+    for j in range(len(items)):
+        if i ==j:
+            #creating the histograms with 20 bins each
+            axs[i,j].hist(strat_data_train[items[i]], bins=20)
+        else:
+                #creating the scatter plots
+                axs[i,j].scatter(strat_data_train[items[j]], strat_data_train[items[i]], s=4)
+        if j==0:
+                #this part is used to label the axes
+                axs[i,j].set_ylabel(items[i])
+        else:
+                    #removes the y-ticks except those on the first column
+                    axs[i,j].set_yticks([])
+        if i == len(items) - 1:
+                        #if the subplot is in the last row, the x-axis label is the name at position j
+                        axs[i, j].set_xlabel(items[j])
+        else:
+                            #removes x-axis ticks except those on the last row
+                            axs[i, j].set_xticks([])
+
+                  
